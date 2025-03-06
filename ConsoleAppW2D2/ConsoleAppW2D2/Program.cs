b265e8cbce6e374ee472fe25d0072d8b5e1e09a1
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-06 11:24:57 +0100

    commit

--- ConsoleAppW2D2/ConsoleAppW2D2/Program.cs
+++ ConsoleAppW2D2/ConsoleAppW2D2/Program.cs
@@ -1,21 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleAppW2D2.Models;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-var Curriculum = new Curriculum();
+        var Curriculum = new Curriculum();
 
-var Info = new Curriculum.Informazioni_Personali();
+        var Info = new Curriculum.Informazioni_Personali();
 
-Console.WriteLine("Scrivi il Tuo Nome");
+        Console.WriteLine("Scrivi il Tuo Nome");
 
-Info.Nome = Console.ReadLine();
+        Info.Nome = Console.ReadLine();
 
-Console.WriteLine("Scrivi il Tuo Cognome");
+        Console.WriteLine("Scrivi il Tuo Cognome");
 
-Info.Cognome = Console.ReadLine();
+        Info.Cognome = Console.ReadLine();
 
-Console.WriteLine("Digita il tuo numero(+39 gia incluso)");
+        Console.WriteLine("Scrivi la tua mail");
 
-Info.Telefono =
+        Info.Email = Console.ReadLine();
 
+        Console.WriteLine("Digita il tuo numero(+39 gia incluso)");
+
+        Info.Telefono = 
+
+
+
+        Console.WriteLine($"CV di {Info.Nome} {Info.Cognome}");
+        Console.WriteLine("");
+        Console.WriteLine("++++ INIZIO Informazioni Personali: ++++");
+        Console.WriteLine($"Nome:{Info.Nome}");
+        Console.WriteLine($"Cognome:{Info.Cognome}");
+        Console.WriteLine($"Email:{}");
+        Console.WriteLine($"Telefono: +39{Info.Telefono}");
+
+    }
+}
