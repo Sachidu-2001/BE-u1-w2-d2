d57007d3ea959f2450640b385c60a0bbda8e4185
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-06 10:42:24 +0100

    commit

---
+++ ConsoleAppW2D2/ConsoleAppW2D2/Models/Curriculum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppW2D2.Models
+{
+    internal class Curriculum
+    {
+        internal class Informazioni_Personali
+        {
+            internal string Nome { get; set; } = "";
+            internal string Cognome { get; set; } = "";
+            internal int Telefono { get; set; } = "";
+            internal string Email { get; set; } = "";
+        }
+        internal class Studi
+        {
+            internal string Qualifica { get; set; } = "";
+            internal string Istituto { get; set; } = "";
+            internal string Tipo { get; set; } = "";
+            internal DateTime Dal;
+            internal DateTime Al;
+        }
+        internal class Impiego
+        {
+            internal string Esperienza { get; set; } = "";
+        }
+        internal class Esperienza
+        {
+            internal string Azienda { get; set; } = "";
+            internal string JobTitle { get; set; } = "";
+            internal DateTime Dal;
+            internal DateTime Al;
+            internal string Descrizione { get; set; } = "";
+            internal string Compiti { get; set; } = "";
+
+        }
+    }
+
+    
+}
---
+++ ConsoleAppW2D2/ConsoleAppW2D2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppW2D2.Models;
+
+Console.WriteLine("Hello, World!");
+
+var Curriculum = new Curriculum();
+
+var Info = new Curriculum.Informazioni_Personali();
+
+Console.WriteLine("Scrivi il Tuo Nome");
+
+Info.Nome = Console.ReadLine();
+
+Console.WriteLine("Scrivi il Tuo Cognome");
+
+Info.Cognome = Console.ReadLine();
+
+Console.WriteLine("Digita il tuo numero(+39 gia incluso)");
+
+Info.Telefono =
+
