c83fb761fbbf28dee60b0d80d22f06c30bd7303f
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-12 17:00:03 +0100

    commit

---
+++ EpicLibrary/EpicLibrary/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+
+namespace EpicLibrary.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength]
+        public string Autore {  get; set; }
+        [Required]
+        [MaxLength]
+        public string Genere { get; set; }
+        [Required]
+        [MaxLength]
+        public string Disponibilità { get; set; }
+
+
+
+
+
+
+    }
+}
