d8139bd498954ba627a4bc010a4bd0ef28647a9e
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-14 12:12:41 +0100

    commit

---
+++ WebAppEsame2/WebAppEsame2/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using WebAppEsame2.Models;
+
+namespace WebAppEsame2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+        
+        public DbSet<Anagrafica> Anagrafiche { get; set; }
+        public DbSet<Tipo_Violazione>  Tipo_Violazioni {  get; set; }
+        public  DbSet<Verbale> Verbali { get; set; }
+    }
+}
---
+++ WebAppEsame2/WebAppEsame2/Models/Tipo_Violazione.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppEsame2.Models
+{
+    public class Tipo_Violazione
+    {
+        [Key]
+        public int Id_Violazione { get; set; }
+        [Required]
+        public int Descrizione { get; set; }
+
+
+    }
+}
